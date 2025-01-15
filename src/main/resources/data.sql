76332bf3ae2ebf248f0f8be8092936ef798d405b
manalizo <yumimanal8@gmail.com>
2025-01-15 12:18:00 +0100

    h2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 INSERT INTO roles (name)
 VALUES ('ROLE_USER'),
        ('ADMIN');
+INSERT INTO users  (email,password)
+VALUES ('manal@gmail.com','$2a$10$DFAHGU2RrvZ1kZVJ/fM75u6Ip9t2m61BP0XH3B0gAErOpavps398y'),
+       ('admin@gmail.com','$2a$10$LDFd8tUcrYQHPSHLUHDu1OCxadpDalzVhfDFuX2Oc5ZvLnmtS2Zx2'),
+       ('sanae@gmail.com','$2a$10$Z3OwKa9Kj1akrRpsOSHdd.9bdiHVFvnKWE5UM6HV85QZzsv1u.aGK');
+INSERT INTO user_roles (user_id,role_id)
+VALUES (1,1),
+       (2,2),(3,1);
